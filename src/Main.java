ea2a5c7b6d757926e77cf613882e521c61231b97
RedChiken <gombabo1217@gmail.com>
2018-06-07 00:29:22 +0900

    print result
    1. start state가 표시되어 있지 않음
    2. 충분히 탐색하지 않음
    3. state가 중복 될 경우 어떻게 못한다

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
@@ -10,16 +11,31 @@ class Container{
     }
 
     public boolean isEnd(){
-        return syntax.length() - index > 0;
+        return syntax.length() == index;
     }
 
     public Container getNextOne(){
         if(isEnd()){
-            return null
+            return null;
         }
         else{
             return new Container(syntax, index + 1);
         }
+    }
+
+    public String getFullSyntax(){
+
+        if(index == 0){
+            return "." + syntax;
+        }
+        else if(index == syntax.length() - 1){
+            return syntax + ".";
+        }
+        else{
+            return syntax.substring(0, index - 1) + "." + syntax.substring(index - 1, syntax.length() );
+        }
+        //return syntax.substring(index - 1) + "." + syntax.substring(index, syntax.length() - 1);
+
     }
 }
 
@@ -28,8 +44,7 @@ public class Main {
     static HashMap<String, ArrayList<String>> syntaxes = new HashMap<String, ArrayList<String>>();
     static HashMap<String, String> rule = new HashMap<>();
     static ArrayList<String> keyList = new ArrayList<String>();
-    static HashMap<Container, ArrayList<Container>> iZero = new HashMap<Container, ArrayList<Container>>();
-
+    static ArrayList<ArrayList<Container>> iZero = new ArrayList<ArrayList<Container>>();
 
     public static void main(String[] args) {
         try {
@@ -44,6 +59,15 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        c0();
+        for(ArrayList<Container> list : iZero){
+            System.out.print("I>");
+            for(Container iter : list){
+                System.out.print("[" + iter.getFullSyntax() + "]");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void readFile(String file) throws IOException{
@@ -64,16 +88,63 @@ public class Main {
 
     public static void c0(){
         Container start = new Container("S->" + keyList.get(0), 0);
-        iZero.put(start, new ArrayList<>());
+        iZero.add(new ArrayList<>());
         for(String key : keyList){
             for(String value : syntaxes.get(key)){
-                iZero.get(start).add(new Container(value, 0));
-                String syntaxkey = Character.toString(value.charAt(0));
-                if (keyList.contains(syntaxkey)){
+                iZero.get(0).add(new Container(value, 0));
+            }
+        }
+        //fix it
+        int length;
+        int index = 0;
+        do{
+            length = iZero.size();
+            i0(iZero.get(index++));
+        }while(iZero.size() != length);
+    }
+
+    public static void i0(ArrayList<Container> list){
+        HashMap<String, ArrayList<Container>> input = new HashMap<>();
+        for(Container c : list){
+            //error
+            String state;
+            if(c.isEnd()){
+                continue;
+            }
+            else{
+                state = Character.toString(c.syntax.charAt(c.index));
+            }
+            if(!input.containsKey(state)){
+                //add ararylist if this state is first time
+                input.put(state, new ArrayList<>());
+            }
+            ArrayList<Container> temp = input.get(state);
+            Container nextone = c.getNextOne();
+            if(nextone != null){
+                //check if string is ended
+                ArrayList<Container> value = existOnIZero(nextone);
+                if(value != null){
+                    //add exist data
+                    temp.addAll(value);
+                }
+                else{
+                    //add next state
+                    temp.add(nextone);
                 }
             }
         }
+        for(String str : input.keySet()){
+            iZero.add(input.get(str));
+        }
+    }
 
+    public static ArrayList<Container> existOnIZero(Container c){
+        for(ArrayList<Container> inter : iZero){
+            if(inter.get(0).equals(c)){
+                return inter;
+            }
+        }
+        return null;
     }
 
     public static void i(String str){
